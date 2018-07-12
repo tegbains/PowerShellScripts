8ebe009594447429937d55f59371ebaaba397ddc
Teg Bains <tegbains@tegbains.com>
2018-07-11 22:53:24 -0700

    Initial Commit

---
+++ Exchange/exportMailboxToPST.ps1
@@ -0,0 +1,7 @@
+﻿$targetMailbox = Read-Host -Prompt 'Input your mailbox name'
+
+$mailboxExportPath = "\\scdc-ex02\OldEmailDumps\"
+
+$targetMailboxPath = $mailboxExportPath + $targetMailbox + ".pst"
+
+New-MailboxExportRequest –Mailbox $targetMailbox –FilePath $targetMailboxPath 
